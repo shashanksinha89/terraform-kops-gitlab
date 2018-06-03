eed092b8238fc717f8172ff6cf888bb43dfc8218
Shashank Sinha <shashank.sinha@starzplay.com>
2018-06-03 21:38:09 +0400

    add code

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "production_database_name" {
+  description = "The database name for Production"
+}
+
+variable "production_database_username" {
+  description = "The username for the Production database"
+}
+
+variable "production_database_password" {
+  description = "The user password for the Production database"
+}
